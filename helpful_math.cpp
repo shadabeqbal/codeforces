1098f2d825a72436dc116b0426afa1b6299eff8a
Shadab Eqbal <shadabeqbal2008@gmail.com>
2020-05-28 20:52:59 +0530

    commit

---
+++ helpful_math.cpp
@@ -0,0 +1,40 @@
+//Author: Shadab Eqbal
+//http://codeforces.com/problemset/problem/282/A
+
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <map>
+#include <stack>
+#include <queue>
+#include <iterator>
+#include <climits>
+#include <string>
+#include <math.h>
+
+#define MAX 1e18
+#define MIN -1e18
+#define MOD 1000000007
+#define PB push_back
+#define MP make_pair
+#define vi vector<int>
+#define vii vector<vector<int>>
+#define pi pair<int, int>
+#define vpi vector<pi>
+#define ll long long int
+#define vll vector<ll>
+
+using namespace std;
+
+int main()
+{
+    string str;
+    cin>>str;
+    sort(str.begin(),str.end());
+    int size=str.length();
+    string temp=str.substr(size/2,size);
+    for(int i=1;i<int(str.length());i+=2){
+        temp.insert(i,1,'+');
+    }
+    cout<<temp;
+}
