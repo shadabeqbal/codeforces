4909f3e68a6e50934ef12f51449d20a5dcc9a714
Shadab Eqbal <shadabeqbal2008@gmail.com>
2020-05-30 00:50:02 +0530

    few optimizations

--- beautifulmatrix.cpp
+++ beautifulmatrix.cpp
@@ -6,7 +6,7 @@
    //  \\
 Author: Shadab Eqbal 
 Created on:30-05-2020
-//http://codeforces.com/problemset/problem/263/A
+Link: http://codeforces.com/problemset/problem/263/A
 */
 
 #include <iostream>
@@ -37,32 +37,23 @@ using namespace std;
 
 int main()
 {   
-    int row=12;
-    int col=12;
+    int row=6; //row=row+1
+    int col=6; //col=col+1
     int mid_row=ceil(row/2);
     int mid_col=ceil(col/2);
     int mat[row][col];
-    pair<int, int> pr;
+    int x = 0, y = 0;
     for (int i = 1; i <= row-1; i++)
     {
         for (int j = 1; j <= col-1; j++)
         {
             cin >> mat[i][j];
             if (mat[i][j] == 1){
-                pr.first=i;
-                pr.second=j;
+                x = abs(i - mid_row);
+                y = abs(j - mid_col);
             }
         }
     }
     
-    int x = 0, y = 0;
-    for (int i = 1; i <= row-1; i++)
-    {
-        for (int j = 1; j <= col-1; j++)
-        {
-            x = abs(pr.first - mid_row);
-            y = abs(pr.second - mid_col);
-        }
-    }
     cout << x + y;
 }
